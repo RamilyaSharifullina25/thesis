32bee7a838c0246df5f38f3a7ed9600790167217
RamilyaSharifullina25 <sharifullina.ramilya@bk.ru>
2022-05-21 08:20:01 +0300

    renewed something

--- TIME/dataset.py
+++ TIME/dataset.py
@@ -1,15 +1,9 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
 
-class DataSet():
+class Main_dataset(Dataset):
     def __init__(self, data, labels, feature_scales={}, N=25000, length_lim=90, sparse_rate=1):
-
-#         if len(data) > N:
-#             sub_inds = np.random.choice(len(data), N, replace=False)
-#         else:
-#             sub_inds = np.arange(len(data))
         
         self.data = data
         self.data = torch.tensor(self.data).to(torch.float32)
@@ -33,3 +27,50 @@ class DataSet():
 
     def __getitem__(self, i):
         return self.data[i], self.labels[i]
+    
+
+class Verification_dataset(Dataset):
+    def __init__(self, needed, feature_scales_simil={}, feature_scales_diff={}):
+        
+        self.needed = needed
+        self.feature_scales_simil = feature_scales_simil
+        self.feature_scales_diff = feature_scales_diff
+        
+    def __getitem__(self, index):
+
+        time1 = self.needed[index][0]
+        time2 = self.needed[index][1]
+        relation = self.needed[index][2]
+        
+        self.time1 = torch.tensor(time1).to(torch.float32)
+        self.time2 = torch.tensor(time2).to(torch.float32)
+        self.relation = torch.tensor(relation).to(torch.float32)
+        
+        # time1
+        for f in range(self.time1.shape[1]):
+            self.time1[:, f] = self.time1[:, f] - self.time1[:, f].mean()
+            
+        for f in range(self.time1.shape[1]):
+            if f not in self.feature_scales_simil:
+                std = self.time1[:, f].std()
+                self.feature_scales_simil[f] = std
+            self.time1[:, f] = self.time1[:, f] / self.feature_scales_simil[f]
+            
+        # time2
+        for f in range(self.time2.shape[1]):
+            self.time2[:, f] = self.time2[:, f] - self.time2[:, f].mean()
+            
+        for f in range(self.time2.shape[1]):
+            if f not in self.feature_scales_diff:
+                std = self.time2[:, f].std()
+                self.feature_scales_diff[f] = std
+            self.time2[:, f] = self.time2[:, f] / self.feature_scales_diff[f]
+            
+        self.time1 = self.time1.transpose(0, 1)
+        self.time2 = self.time2.transpose(0, 1)
+
+        return self.time1, self.time2, self.relation
+
+    def __len__(self):
+        return len(self.needed)
+    
